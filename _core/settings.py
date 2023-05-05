888d0dfa3f7c25c98579424cbdcfa3ac082bb1d2
renangiaretta <renan.giaretta@hotmail.com>
2023-05-05 08:19:31 -0300

    feat: create app users

--- _core/settings.py
+++ _core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
---
+++ users/views.py
@@ -0,0 +1,13 @@
+from .models import User
+from .serializers import UserSerializer
+from rest_framework import generics
+
+
+class UserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
