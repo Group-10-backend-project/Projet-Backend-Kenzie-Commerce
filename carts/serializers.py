f20419232a7aae76dc84368ff6eee9fb7ee92b85
unknown <jonatasilveira04@gmail.com>
2023-05-09 10:27:58 -0300

    chage route cart

--- carts/serializers.py
+++ carts/serializers.py
@@ -4,31 +4,18 @@ from users.models import User
 from products.models import Product
 from users.serializers import UserSerializer
 from products.serializers import ProductSerializer
-import ipdb
-
-
-class CartSerializer(serializers.ModelSerializer):
-    products = ProductSerializer(many=True)
-    user = UserSerializer()
-
-    class Meta:
-        model = Cart
-        fields = ['is_active', 'user', 'products']
-        extra_kwargs = {
-            'is_active': {'read_only': True}
-        }
+from django.shortcuts import get_object_or_404
 
 
 class CartProductSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
-
-        user_id = validated_data.pop('user_id')
-        user = User.objects.get(id=user_id)
-
-        cart = Cart.objects.filter(user=user, is_active=True)[0]
+        user = validated_data.pop('user')
+        cart = Cart.objects.filter(user=user, is_active=True)
 
         if not cart:
             cart = Cart.objects.create(user=user)
+        else:
+            cart = cart[0]
 
         validated_data['cart'] = cart
 
@@ -41,4 +28,16 @@ class CartProductSerializer(serializers.ModelSerializer):
         fields = ['amount', 'user', 'products_id']
         extra_kwargs = {
             'products_id': {'source': 'products'}
+        }
+
+
+class CartSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+    user = UserSerializer()
+
+    class Meta:
+        model = Cart
+        fields = ['is_active', 'user', 'products']
+        extra_kwargs = {
+            'is_active': {'read_only': True}
         }
--- carts/urls.py
+++ carts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/<int:user_id>/cart/', views.CartView.as_view())
+    path('cart/', views.CartView.as_view()),
+    path('users/<int:user_id>/cart/', views.CartDetailView.as_view())
 ]
